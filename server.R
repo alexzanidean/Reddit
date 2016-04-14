7cb6517d1312c702583e0a28c1861b62426f50b2
alexzanidean <alex.zanidean@uleth.ca>
2016-04-14 09:17:25 -0600

    Added files via upload

--- server.R
+++ server.R
@@ -1,22 +1,25 @@
-shinyServer(function(input, output, sesson) {
-    
-output$plotreddit <- renderPlot({
-  redditdata <- reddit_content(URL=input$urlinput)
-  data.text <- redditdata$comment
-  data.text <- clean_text(data.text)
-  data_corpus = Corpus(VectorSource(data.text))
-  #data_corpus <- tm_map(data_corpus,
-  #                      content_transformer(function(x) iconv(x, to='UTF-8', sub='byte')), mc.cores=1, lazy=TRUE)
-  #data_corpus <- tm_map(data_corpus, content_transformer(tolower), lazy = TRUE)
-  data_corpus <- tm_map(data_corpus, function(x)removeWords(x,stopwords('english')), lazy = TRUE)
-  data_corpus <- tm_map(data_corpus, function(x)removeWords(x,stopwords("french")), lazy= TRUE)
-  #data_corpus <- tm_map(data_corpus, stemDocument, mc.cores=1)
-  
-  myDTM = TermDocumentMatrix(data_corpus, control = list(minWordLength = 0))
-  m = as.matrix(myDTM)
-  v = sort(rowSums(m), decreasing = TRUE)
-  
-  wordcloud(names(v),v ,min.freq=5,max.words=300, random.order=F, colors=pal2, rot.per=.2)
+library(RedditExtractoR)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+library(shiny)
+pal2 <- brewer.pal(8,"Dark2")
+
+shinyServer(function(input, output, session) {
+  output$plotreddit <- renderPlot({
+    redditdata <- reddit_content(URL=input$urlinput)
+    reddit.text <- redditdata$comment
+    reddit_corpus = Corpus(VectorSource(reddit.text))
+    myDTM = TermDocumentMatrix(
+      reddit_corpus,
+      control = list(
+      wordLengths=c(0,Inf),
+      removePunctuation = TRUE,
+      stopwords = c(stopwords("english")),
+      removeNumbers = TRUE, tolower = TRUE) )
+    m = as.matrix(myDTM)
+    v = sort(rowSums(m), decreasing = TRUE)
+    wordcloud(names(v),v,colors=pal2, min.freq = input$freqnumber, max.words = input$cloudnumber)
       })
 
 })
--- ui.R
+++ ui.R
@@ -2,10 +2,12 @@ shinyUI(pageWithSidebar(
                   headerPanel("Reddit WordCloud"),
                   
                   sidebarPanel(
-                    textInput("urlinput", "URL", "Comment Thread"),
+                    textInput("urlinput", "URL", "https://www.reddit.com/r/todayilearned/comments/4eihm3/til_dogs_feel_most_vulnerable_when_pooping_so/"),
+                    sliderInput("cloudnumber", "Number of words in cloud", min = 50, max = 500, value = 200),
+                    sliderInput("freqnumber", "Min word Frequency", min = 1, max = 25, value = 5),
                     submitButton(text = "Update")),
 
                   mainPanel(
-                    plotOutput("plotreddit", width = "100%"))
+                    plotOutput("plotreddit"))
               )
           )
