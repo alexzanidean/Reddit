0c1390cec2ac2e15ff5d3b0d22afd55ead75efcc
alexzanidean <alex.zanidean@uleth.ca>
2016-04-14 09:19:49 -0600

    Added files via upload

--- Wordcloud.R
+++ Wordcloud.R
@@ -1,23 +1,20 @@
 library(RedditExtractoR)
-library(wordcloud)
 library(tm)
+library(wordcloud)
 library(RColorBrewer)
-pal2 <- brewer.pal(8,"Dark2")
 
-
-data <- reddit_content(URL=
-  "https://www.reddit.com/r/The_Donald/comments/4ch9xa/im_gonna_kick_your_kid_in_the_ribs_for_dressing/")
-data.text <- data$comment
-data_corpus = Corpus(VectorSource(data.text))
-#data_corpus <- tm_map(data_corpus,
-#                      content_transformer(function(x) iconv(x, to='UTF-8', sub='byte')),                       mc.cores=1, lazy=TRUE)
-#data_corpus <- tm_map(data_corpus, content_transformer(tolower), lazy = TRUE)
-data_corpus <- tm_map(data_corpus, function(x)removeWords(x,stopwords('english')), lazy = TRUE)
-data_corpus <- tm_map(data_corpus, function(x)removeWords(x,stopwords("french")), lazy= TRUE)
-#data_corpus <- tm_map(data_corpus, stemDocument, mc.cores=1)
-
-myDTM = TermDocumentMatrix(data_corpus, control = list(minWordLength = 3))
+pal2 <- brewer.pal(8,"
+redditdata <- reddit_content(URL= )
+reddit.text <- redditdata$
+reddit_corpus = Corpus(VectorSource(reddit.
+myDTM = TermDocumentMatrix(
+      reddit_corpus,
+      control = list(
+      wordLengths=c(0,Inf),
+      removePunctuation = TRUE,
+      stopwords = c(stopwords("english")),
+      removeNumbers = TRUE, 
 m = as.matrix(myDTM)
 v = sort(rowSums(m), decreasing = TRUE)
+wordcloud(names(v),v,colors=pal2, min.freq = 5, max.words = 200)
 
-wordcloud(names(v),v ,min.freq=3,max.words=200, random.order=F, colors=pal2, rot.per=.2)
